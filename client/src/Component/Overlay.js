26c53cf7746ff073a133fb83600509dc8a1c5bfe
Pavel <pavel.filkovskiy@gmail.com>
2021-12-25 23:56:03 +0000

    add couple content viewers

--- client/src/Component/Overlay.js
+++ client/src/Component/Overlay.js
@@ -1,10 +1,12 @@
 import "./Overlay.css";
+import Viewer from "./ViewerFactory";
 
 export default function Overlay({ url, closeOverlay }) {
-  return <div className="overlay-wrapper" onClick={closeOverlay}>
+  return <div className="overlay-wrapper">
     <div className="overlay-backdrop">
+      <div className="overlay-close-btn" onClick={closeOverlay}>close</div>
       <div className="overlay-content">
-        {url}
+        <Viewer url={url}/>
       </div>
     </div>
   </div>
