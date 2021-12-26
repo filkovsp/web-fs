f5b2bc1377142a0e0fa0cbc969b39e5df83ad3bc
Pavel <pavel.filkovskiy@gmail.com>
2021-12-26 17:18:06 +0000

    add webm support

--- client/src/Component/ViewerFactory.js
+++ client/src/Component/ViewerFactory.js
@@ -1,38 +1,40 @@
 import ResponsivePlayer from 'react-player';
+import "./Viewer.css";
 
 export default function Viewer ({url}) {
-  const ext = url.split(".").at(-1);
-  switch (ext) {
-    // case "pdf":
+  const fileExt = url.split(".").at(-1);
+  switch (fileExt) {
+    // todo: add support for pdf files:
     case "bmp":
     case "png":
     case "tiff":
     case "jpg":
     case "jpeg":
-      return <div style={{
-        width: "100%",
-        height: "100%",
-        display: "flex",
-        "align-items": "center",
-        "justify-content": "center"
-      }}>
+      return <div className="viewer">
         <img alt={url.split("path=").at(1)} src={url}/>
       </div>
 
     case "mp3":
     case "mp4":
-      return <ResponsivePlayer
-        url={url}
-        playing={false}
-        controls={true}
-        width='100%'
-        height='100%'
-      />
+    case "webm":
+    case "mkv":
+      return <div className="viewer">
+        <div className="title">{url.split("/").at(-1)}</div>
+        <ResponsivePlayer
+          url={url}
+          playing={true}
+          controls={true}
+          width='90%'
+          height='90%'
+        />
+      </div>
 
     default:
-      return <div>
-        couldn't found an appropriate content vewer for<br/>
-        <a rel="noreferrer" href={url} target="_blank">{url.split("path=").at(1)}</a>
+      return <div className="viewer">
+        couldn't find an appropriate content viewer for<br/>
+        <span className="link" onClick={() => window.open(url, "_blank")}>
+          {url.split("path=").at(1)}
+        </span>
       </div>
   }
 }
